af4056a2c5c6842ba1c35f4fb811e3811f2bb715
Saheed <saheedsh2@gmail.com>
2023-01-05 14:41:37 +0100

    first commit

---
+++ src/components/Filters.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Form } from "react-bootstrap";
+
+const Filters = () => {
+  return (
+    <div className="filters">
+      <span className="title">Filter Products</span>
+      <span>
+        <Form.Check
+          inline
+          label="Ascending"
+          name="group1"
+          type="radio"
+          id={`inline-1`}
+        />
+      </span>
+      <span>
+        <Form.Check
+          inline
+          label="Descending"
+          name="group1"
+          type="radio"
+          id={`inline=1`}
+        />
+      </span>
+    </div>
+  );
+};
+
+export default Filters;
